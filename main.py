1c46c8ce31aae45b26b3c964d6eda3134c41fd05
Ramon Echeverria gonzales <rieg@MacBook-Pro-de-Ramon.local>
2020-05-22 18:20:50 -0400

    changed parameter and variable names in main.py

--- main.py
+++ main.py
@@ -10,19 +10,18 @@ sources = ['solar', 'wind', 'hydroelectric', 'nuclear', 'gas', 'coal', 'oil'] # 
 periods = [i for i in range(1, N_PERIODS_YEAR+1)] # conjunto _t
 
 # parametros constantes
-budget = 700000000000 # presupuesto en dolares # 7000000000000
+P = 700000000000 # presupuesto en dolares # 7000000000000
 
-years_return = 40 # cantidad de anos en los que se espera recuperar la inversion inicial
+pi = 40 # cantidad de anos en los que se espera recuperar la inversion inicial
 
-demand = Synthesize().synthesize_data(N_PERIODS_YEAR) # diccionario tipo {periodo:demanda}
+D_t = Synthesize().synthesize_data(N_PERIODS_YEAR) # diccionario tipo {periodo:demanda}
 
-battery_cost = 103240 # costo por bateria en dolares AC
-battery_capacity = 232 # capacidad de almacenamiento en kWh por bateria AQ
-battery_efficiency = 0.87 # eficiencia de energia de las baterias AE
-pollution_per_battery = 46400 # kgs de CO2 emitidos por bateria producida AZ
+AC = 103240 # costo por bateria en dolares
+AQ = 232 # capacidad de almacenamiento en kWh por bateria
+AE = 0.87 # eficiencia de energia de las baterias
+AZ = 46400 # kgs de CO2 emitidos por bateria producida
 
-kwh_price_mean = 0.1 # valor por kWh en el mercado promedio en dolares
-kwh_price_chile = 0.16 # valor por kWh en chile en dolares
+S = 0.1 # valor por kWh en el mercado promedio en dolares
 
 # parametro CI_i costo inicial por kW de capacidad en dolares
 overnight_cost = {
@@ -36,7 +35,7 @@ overnight_cost = {
     }
 
 # modelaremos ocupando la media en casos donde hayan mas de un dato
-overnight_cost_mean = {
+CI_i = {
     key:np.mean(value) for key, value in  zip(overnight_cost.keys(),
                                               overnight_cost.values())
 }
@@ -53,13 +52,13 @@ production_cost = {
 }
 
 # al igual que el costo inicial, ocuparemos la media
-production_cost_mean = {
+C_i = {
     key:np.mean(value) for key, value in zip(production_cost.keys(),
                                             production_cost.values())
 }
 
 # kg de CO2 emitidos por producir un kWh Z_i
-kg_CO2_per_kwh = {
+Z_i = {
     'solar': 0.085,
     'wind': 0.026,
     'hydroelectric': 0.026,
@@ -69,67 +68,64 @@ kg_CO2_per_kwh = {
     'oil': 0.733,
     }
 
-# to-do: cantidad maxima disponible para instalaciones QM_i
-# prod_units_limit = {source:limit for source,
-#                     limit in zip(sources, np.array([random.randint(9448046//6, 267628704//6) for _ in
-#                                                                                 range(len(sources))]))}
+QM_i = {source:150000000 for source in sources}
 
-prod_units_limit = {source:150000000 for source in sources}
+### Modelamiento ###
 
 from gurobipy import GRB, Model, quicksum
 
 model = Model('Energy production planning')
 
-prod_units = model.addVars(sources, vtype=GRB.INTEGER, name='prod_units')
-storage = model.addVar(vtype=GRB.INTEGER, name='storage')
-output = model.addVars(periods, sources, vtype=GRB.CONTINUOUS, name='output')
-stored = model.addVars(periods, vtype=GRB.CONTINUOUS, name='stored')
+xi_i = model.addVars(sources, vtype=GRB.INTEGER, name='prod_units')
+a = model.addVar(vtype=GRB.INTEGER, name='storage')
+x_it = model.addVars(periods, sources, vtype=GRB.CONTINUOUS, name='output')
+b_t = model.addVars(periods, vtype=GRB.CONTINUOUS, name='stored')
 
 model.update()
 
 
-initial_investment = quicksum(prod_units[source] * overnight_cost_mean[source]
-                        for source in sources) + storage * battery_cost
+initial_investment = quicksum(xi_i[source] * CI_i[source]
+                        for source in sources) + a * AC
 
 # restriccion 1: inversion inicial menor o igual a presupuesto
-model.addConstr(initial_investment <= budget, name='budget_limit')
+model.addConstr(initial_investment <= P, name='budget_limit')
 
 # restriccion 2: recuperacion de inversion (el presupuesto dividido en la cantidad de anos nos indica
 # cuanto debemos recuperar cada ano)
-profit = quicksum(output[period, source] * (kwh_price_mean - production_cost_mean[source])
+profit = quicksum(x_it[period, source] * (S - C_i[source])
                      for source in sources for period in periods)
 
-model.addConstr((profit >= (initial_investment/years_return)), name='payback')
+model.addConstr((profit >= (initial_investment/pi)), name='payback')
 
 # restriccion 3: satisfaccion de demanda y flujo
-model.addConstrs((stored[period] == quicksum(output[period, source] for source in sources) - demand[str(period)] +
-                                            (stored[period - 1] * battery_efficiency)
+model.addConstrs((b_t[period] == quicksum(x_it[period, source] for source in sources) - D_t[str(period)] +
+                                            (b_t[period - 1] * AE)
                                                 for idx, period in enumerate(periods) if idx != 0),
                                                                                      name='demand_meet')
 
 # restriccion 4: produccion de energia limitada
-model.addConstrs((output[period, source] <= prod_units[source] * PERIOD_LENGTH for source in sources
+model.addConstrs((x_it[period, source] <= xi_i[source] * PERIOD_LENGTH for source in sources
                                                                                 for period in periods),
                                                                                  name='energy_limit')
 # restriccion 5: construccion de kw de capacidad limitadas
-model.addConstrs((prod_units[source] <= prod_units_limit[source] for source in sources), name='prod_limit')
+model.addConstrs((xi_i[source] <= QM_i[source] for source in sources), name='prod_limit')
 
 # restriccion 6: cantidad maxima de almacenamiento
-model.addConstrs((stored[period] <= storage * battery_capacity for period in periods), name='storage_limit')
+model.addConstrs((b_t[period] <= a * AQ for period in periods), name='storage_limit')
 
 # Naturaleza de las variables
 # restriccion 7:
-model.addConstrs((output[period, source] >= 0 for source in sources for period in periods), name='v_nature_7')
+model.addConstrs((x_it[period, source] >= 0 for source in sources for period in periods), name='v_nature_7')
 # restriccion 8:
-model.addConstrs((prod_units[source] >= 0 for source in sources), name='v_nature_8')
+model.addConstrs((xi_i[source] >= 0 for source in sources), name='v_nature_8')
 # restriccion 9:
-model.addConstr(storage >= 0, name='v_nature_9')
+model.addConstr(a >= 0, name='v_nature_9')
 # restriccion 10 y 11:
-model.addConstrs((stored[period] >= 0 for idx, period in enumerate(periods) if idx != 0), name='v_nature_10')
-model.addConstrs((stored[period] == 0 for idx, period in enumerate(periods) if idx == 0), name='v_nature_11')
+model.addConstrs((b_t[period] >= 0 for idx, period in enumerate(periods) if idx != 0), name='v_nature_10')
+model.addConstrs((b_t[period] == 0 for idx, period in enumerate(periods) if idx == 0), name='v_nature_11')
 
-obj = quicksum(output[period, source] * kg_CO2_per_kwh[source]\
-               for source in sources for period in periods) + storage * pollution_per_battery
+obj = quicksum(x_it[period, source] * Z_i[source]\
+               for source in sources for period in periods) + a * AZ
 
 model.setObjective(obj, GRB.MINIMIZE)
 
