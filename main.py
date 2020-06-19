1403d592522737b122faa0ef7de968380f538e4a
Ramon Echeverria gonzales <rieg@uc.cl>
2020-06-18 21:45:41 -0400

    fixed battery storage parameter error and made all attrs static

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from data import Synthesize
 from gurobipy import GRB, quicksum
 from gurobipy import Model as GurobiModel
+from os import path
 
 class Model(GurobiModel):
     Y = 6 # numero de horas de cada periodo
@@ -12,62 +13,63 @@ class Model(GurobiModel):
     sources = ['solar', 'wind', 'hydroelectric', 'nuclear', 'gas', 'coal', 'oil'] # conjunto _i
     periods = [i for i in range(1, N_PERIODS_YEAR+1)] # conjunto _t
 
+    P = 700000000000 # presupuesto en dolares
+    pi = 40 # cantidad de anos en los que se espera recuperar la inversion inicial
+    AC = 103240 # costo por bateria en dolares
+    AQ = Y * 232 # capacidad de almacenamiento en kWh por bateria por periodo
+    AE = 0.87 # eficiencia de energia de las baterias
+    AZ = 46400 # kgs de CO2 emitidos por bateria producida
+    S = 0.1 # valor por kWh en el mercado promedio en dolares
+
+    # parametro CI_i costo inicial por kW de capacidad en dolares
+    overnight_cost = {
+        'solar': [1331, 1313, 1100],
+        'wind': [1100, 1500, 1319],
+        'hydroelectric': [5316, 1750, 7500, 3500],
+        'nuclear': [6041, 6317, 6900, 12000],
+        'gas': [950, 713, 1084],
+        'coal': [3000, 6250, 5876, 3676],
+        'oil': [1170, 1175, 713],
+        }
+
+    # modelaremos ocupando la media en casos donde hayan mas de un dato
+    CI_i = {
+        key:np.mean(value) for key, value in  zip(overnight_cost.keys(),
+                                                  overnight_cost.values())
+    }
+
+    # parametro C_i costo de produccion por kWh en dolares
+    production_cost = {
+        'solar': [0.0017, 0.00136],
+        'wind': [0.003, 0.0032],
+        'hydroelectric': [0.005],
+        'nuclear': [0.0210, 0.0138, 0.016],
+        'gas': [0.021],
+        'coal': [0.0098, 0.014, 0.0173],
+        'oil': [0.054, 0.0701, 0.0563],
+    }
+
+    # al igual que el costo inicial, ocuparemos la media
+    C_i = {
+        key:np.mean(value) for key, value in zip(production_cost.keys(),
+                                                production_cost.values())
+    }
+
+    # kg de CO2 emitidos por producir un kWh Z_i
+    Z_i = {
+        'solar': 0.085,
+        'wind': 0.026,
+        'hydroelectric': 0.026,
+        'nuclear': 0.029,
+        'gas': 0.499,
+        'coal': 0.888,
+        'oil': 0.733,
+        }
+
+    QM_i = {source:150000000 for source in sources}
+
     def __init__(self, name):
         super().__init__(name)
-        self.P = 700000000000 # presupuesto en dolares
-        self.pi = 40 # cantidad de anos en los que se espera recuperar la inversion inicial
-        self.AC = 103240 # costo por bateria en dolares
-        self.AQ = 232 # capacidad de almacenamiento en kWh por bateria
-        self.AE = 0.87 # eficiencia de energia de las baterias
-        self.AZ = 46400 # kgs de CO2 emitidos por bateria producida
-        self.S = 0.1 # valor por kWh en el mercado promedio en dolares
-
-        # parametro CI_i costo inicial por kW de capacidad en dolares
-        overnight_cost = {
-            'solar': [1331, 1313, 1100],
-            'wind': [1100, 1500, 1319],
-            'hydroelectric': [5316, 1750, 7500, 3500],
-            'nuclear': [6041, 6317, 6900, 12000],
-            'gas': [950, 713, 1084],
-            'coal': [3000, 6250, 5876, 3676],
-            'oil': [1170, 1175, 713],
-            }
-
-        # modelaremos ocupando la media en casos donde hayan mas de un dato
-        self.CI_i = {
-            key:np.mean(value) for key, value in  zip(overnight_cost.keys(),
-                                                      overnight_cost.values())
-        }
-
-        # parametro C_i costo de produccion por kWh en dolares
-        production_cost = {
-            'solar': [0.0017, 0.00136],
-            'wind': [0.003, 0.0032],
-            'hydroelectric': [0.005],
-            'nuclear': [0.0210, 0.0138, 0.016],
-            'gas': [0.021],
-            'coal': [0.0098, 0.014, 0.0173],
-            'oil': [0.054, 0.0701, 0.0563],
-        }
-
-        # al igual que el costo inicial, ocuparemos la media
-        self.C_i = {
-            key:np.mean(value) for key, value in zip(production_cost.keys(),
-                                                    production_cost.values())
-        }
-
-        # kg de CO2 emitidos por producir un kWh Z_i
-        self.Z_i = {
-            'solar': 0.085,
-            'wind': 0.026,
-            'hydroelectric': 0.026,
-            'nuclear': 0.029,
-            'gas': 0.499,
-            'coal': 0.888,
-            'oil': 0.733,
-            }
-
-        self.QM_i = {source:150000000 for source in self.sources}
 
     def __setattr__(self, attr, value):
         super().__dict__[attr] = value
@@ -124,17 +126,17 @@ class Model(GurobiModel):
         self.setObjective(obj, GRB.MINIMIZE)
         super().optimize()
 
-    def save_results(self, path='gurobi_files'):
-        with open(path + '/slack.text', 'w') as file:
-            for constr in model.getConstrs():
+    def save_results(self, where=''):
+        with open(path.join(where, 'gurobi_files', 'slack.txt'), 'w') as file:
+            for constr in self.getConstrs():
                 file.writelines(f"{constr}: {constr.getAttr('slack')}\n")
             file.close()
 
-        model.write(path + '/model.sol')
+        self.write(path.join(where, 'gurobi_files', 'model.sol'))
 
-        if model.status == GRB.INFEASIBLE:
-            model.computeIIS()
-            model.write(path + '/model.ilp')
+        if self.status == GRB.INFEASIBLE:
+            self.computeIIS()
+            self.write(path.join(where, 'gurobi_files', 'model.ilp'))
 
 if __name__ == '__main__':
     from analysis import plot_demand, plot_results # avoids circular imports
