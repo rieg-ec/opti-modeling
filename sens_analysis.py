ea545e4192d1450ba7cd884af61f5a537e004f7f
Ramon Echeverria gonzales <rieg@uc.cl>
2020-06-19 15:50:19 -0400

    made analysis process more automated and easier

--- sens_analysis.py
+++ sens_analysis.py
@@ -5,12 +5,30 @@ from matplotlib import pyplot as plt
 from matplotlib.ticker import MultipleLocator
 from gurobipy import GRB
 from main import Model
-from analysis import results_plot
+from analysis import plot_results
+from os import path, makedirs
 
 model = Model('Sensibility analysis of original model')
-model.Z_i['wind'] = 2
+# model.AE = 0.95
+# model.AZ *= 0.5
+# model.AC *= 0.8
+# model.AQ *= 2
+# model.P *= 1.5
+#
+# for period in model.D_t:
+#     model.D_t[period] *= 1.2
+# for source in model.sources:
+#     if source in ['coal', 'oil', 'gas']:
+#         model.QM_i[source] *= 0.7
 
 if __name__ == '__main__':
     model.optimize()
-    model.save_results(path='sens_analysis_results/gurobi_files')
-    plot_results(path='sens_analysis_results/plots')
+    folder = 'storage_improves'
+    dirname = path.join('sens_analysis_results', folder)
+    if not path.exists(dirname):
+        plot_dir = path.join(dirname, 'plots')
+        gurobi_files_dir = path.join(dirname, 'gurobi_files')
+        makedirs(plot_dir)
+        makedirs(gurobi_files_dir)
+    model.save_results(where=dirname)
+    plot_results(where=dirname)
